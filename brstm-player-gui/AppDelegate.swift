64932235a453dcfe20c308a6769cecee35b82e86
admin <admin@imac.local>
2020-01-27 16:56:22 +0300

    Initial Commit

---
+++ brstm-player-gui/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  brstm-player-gui
+//
+//  Created by admin on 27/01/20.
+//  Copyright © 2020 FreeAppSW. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ brstm-player-gui/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  brstm-player-gui
+//
+//  Created by admin on 27/01/20.
+//  Copyright © 2020 FreeAppSW. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
