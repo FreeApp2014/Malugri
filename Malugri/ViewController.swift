9a662f5cc82c9296961a69b7f05f2b5947a721cd
Free App <contact@freeappsw.space>
2020-10-19 11:38:00 +0300

    Initial Commit

---
+++ Malugri/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Malugri
+//
+//  Created by Free App on 19/10/2020.
+//  Copyright © 2020 freeappsw. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
